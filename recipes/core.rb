3cf02b65354fad691dd862beb2bebb4078aac2c0
Ben Langfeld <ben@langfeld.me>
2012-09-20 14:32:14 +0100

    Use repoforge to install js and js-devel prerequisites

--- recipes/core.rb
+++ recipes/core.rb
@@ -1,22 +1,9 @@
 package 'zip'
 
-remote_file "/tmp/js185.tar.gz" do
-  source "http://ftp.mozilla.org/pub/mozilla.org/js/js185-1.0.0.tar.gz"
-end
+include_recipe "yum::repoforge"
 
-script "install_spidermonkey" do
-  interpreter "bash"
-  user "root"
-  cwd "/tmp"
-  code <<-EOH
-  tar -zxf js185.tar.gz
-  cd js-1.8.5/js/src
-  autoconf-2.13
-  ./configure
-  make
-  make install
-  EOH
-end
+package 'js'
+package 'js-devel'
 
 yum_repository "LumenVox" do
   name "LumenVox"
