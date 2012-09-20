12f870e8cc9126dd195f76ebedf76e7ecff56ba6
Ben Langfeld <ben@langfeld.me>
2012-09-20 13:06:53 +0100

    Correct name/description

--- recipes/core.rb
+++ recipes/core.rb
@@ -1,5 +1,6 @@
 yum_repository "LumenVox" do
-  name "LumenVox Products $basearch"
+  name "LumenVox"
+  description "LumenVox Products $basearch"
   url "http://www.lumenvox.com/packages/EL6/$basearch/"
   action :add
 end
