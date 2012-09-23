cebe8c0c66703b84907aaa80c5963d11bb67f38f
Ben Langfeld <ben@langfeld.me>
2012-09-23 11:03:38 +0100

    Allow specifying license server credentials

--- recipes/client.rb
+++ recipes/client.rb
@@ -5,8 +5,10 @@ package "LumenVoxClient"
 template "/etc/lumenvox/client_property.conf" do
   source "client_property.conf.erb"
   variables(
-    :license_servers => node['lumenvox']['client']['license_servers'],
-    :sre_servers => node['lumenvox']['client']['sre_servers'],
-    :tts_servers => node['lumenvox']['client']['tts_servers']
+    :license_servers          => node['lumenvox']['client']['license_servers'],
+    :sre_servers              => node['lumenvox']['client']['sre_servers'],
+    :tts_servers              => node['lumenvox']['client']['tts_servers'],
+    :authentication_username  => node['lumenvox']['client']['authentication_username'],
+    :authentication_password  => node['lumenvox']['client']['authentication_password']
   )
 end
