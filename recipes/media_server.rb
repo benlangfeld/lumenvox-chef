fee8b98df6ce6150d40b41e633df87184e55b0c9
Ben Langfeld <ben@langfeld.me>
2012-11-08 14:35:19 -0200

    Don't start media server until config is in place

--- recipes/media_server.rb
+++ recipes/media_server.rb
@@ -2,10 +2,6 @@ include_recipe "lumenvox::core"
 
 package "LumenVoxMediaServer"
 
-service "lvmediaserverd" do
-  action :start
-end
-
 template "/etc/lumenvox/media_server.conf" do
   source "media_server.conf.erb"
   variables(
@@ -13,4 +9,8 @@ template "/etc/lumenvox/media_server.conf" do
     :sre_ip => node['lumenvox']['media_server']['sre_ip'],
     :sip_port => node['lumenvox']['media_server']['sip_port']
   )
+end
+
+service "lvmediaserverd" do
+  action :start
 end
