19c462aa8bf13db98dab63b8d83fc2a106310c2d
Ben Langfeld <ben@langfeld.me>
2014-02-17 12:57:42 -0300

    Add some basic tests to ensure all services get started

--- test/integration/default/serverspec/localhost/lumenvox_spec.rb
+++ test/integration/default/serverspec/localhost/lumenvox_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
 
 describe 'Lumenvox' do
+  describe service('lvttsd') do
+    it { should be_running }
+  end
+
+  describe service('lvsred') do
+    it { should be_running }
+  end
+
+  describe service('lvmediaserverd') do
+    it { should be_running }
+  end
+
+  describe service('lvlicensed') do
+    it { should be_running }
+  end
 end
