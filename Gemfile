a37c1b2c196deab0000a3b0c18814f0b8d560483
Ben Langfeld <ben@langfeld.me>
2014-02-17 12:43:52 -0300

    Update with better basic cookbook layout

--- Gemfile
+++ Gemfile
@@ -1,4 +1,11 @@
-source :rubygems
+source 'https://rubygems.org'
 
-gem 'berkshelf'
-gem 'vagrant', '~> 1.0.5'
+gem 'berkshelf', '~> 2.0'
+
+gem 'foodcritic', '~> 3.0'
+gem 'thor-foodcritic', '~> 1.1'
+
+gem 'test-kitchen', '~> 1.2'
+gem 'kitchen-vagrant'
+
+gem 'knife-cookbook-doc', github: 'mojolingo/knife-cookbook-doc'
--- metadata.rb
+++ metadata.rb
@@ -1,19 +1,19 @@
+name              "lumenvox"
 maintainer        "Mojo Lingo LLC"
 maintainer_email  "ops@mojolingo.com"
 license           "Apache 2.0"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.0.2"
-recipe            "lumenvox", "Installs all Lumenvox components."
-recipe            "lumenvox::core", "Installs core files shared across all packages. This is required."
-recipe            "lumenvox::client", "Installs the Lumenvox speech client."
-recipe            "lumenvox::sre", "Installs the Lumenvox speech server."
-recipe            "lumenvox::license_server", "Installs the Lumenvox license server."
-recipe            "lumenvox::media_server", "Installs the Lumenvox media server which contains both the MRCPv1 and MRCPv2 Server."
-recipe            "lumenvox::tts", "Installs the text-to-speech server, required for a TTS installation."
 
-name              "lumenvox"
+recipe "lumenvox", "Installs all Lumenvox components."
+recipe "lumenvox::core", "Installs core files shared across all packages. This is required."
+recipe "lumenvox::client", "Installs the Lumenvox speech client."
+recipe "lumenvox::sre", "Installs the Lumenvox speech server."
+recipe "lumenvox::license_server", "Installs the Lumenvox license server."
+recipe "lumenvox::media_server", "Installs the Lumenvox media server which contains both the MRCPv1 and MRCPv2 Server."
+recipe "lumenvox::tts", "Installs the text-to-speech server, required for a TTS installation."
 
-depends           "yum"
+depends "yum"
 
 %w{ redhat centos }.each do |os|
   supports os, ">= 6.0"
