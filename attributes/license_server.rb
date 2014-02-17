c274660a2a257a4714dada822947e3401c677b02
Ben Langfeld <ben@langfeld.me>
2014-02-17 13:05:37 -0300

    Split attributes into separate files

---
+++ attributes/license_server.rb
@@ -0,0 +1,1 @@
+default['lumenvox']['license_server']['version'] = '10.5.300-1.el6'
---
+++ attributes/tts.rb
@@ -0,0 +1,20 @@
+default['lumenvox']['tts']['version'] = '10.5.300-2SF.el6'
+default['lumenvox']['tts']['voices']  = [
+  {:voice => "Jason",   :version => '10.5.110-1'},
+  {:voice => "Rebecca", :version => '10.5.110-1'},
+  {:voice => "Rita",    :version => '10.5.110-1'},
+  {:voice => "Sophie",  :version => '10.5.110-1'},
+  {:voice => "Chris",   :version => '10.5.110-1'},
+  {:voice => "Lindsey", :version => '10.5.110-1'},
+  {:voice => "Ben",     :version => '10.5.110-1'},
+  {:voice => "Chloe",   :version => '10.5.110-1'},
+  {:voice => "Lukas",   :version => '10.5.110-1'},
+  {:voice => "Heidi",   :version => '10.5.110-1'},
+  {:voice => "Jean",    :version => '10.5.110-1'},
+  {:voice => "Margot",  :version => '10.5.110-1'},
+  {:voice => "Antonio", :version => '10.5.110-1'},
+  {:voice => "Martina", :version => '10.5.110-1'},
+  {:voice => "Luis",    :version => '10.5.110-1'},
+  {:voice => "Lorena",  :version => '10.5.110-1'},
+  {:voice => "Molly",   :version => '10.5.110-1'}
+]
